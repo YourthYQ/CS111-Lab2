a671e5ee6ab2d44250169bd24e6ff50d9de0c439
Yourth.YQ <sapphireyq@gmail.com>
2024-05-11 15:28:55 -0700

    new

---
+++ rr.c
@@ -0,0 +1,219 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/queue.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+typedef uint32_t u32;
+typedef int32_t i32;
+
+struct process
+{
+  u32 pid;
+  u32 arrival_time;
+  u32 burst_time;
+
+  TAILQ_ENTRY(process) pointers;
+
+  /* Additional fields here */
+  int waiting_time;      // Total time spent waiting in the queue
+  int response_time;     // Time from arrival to first execution
+  int has_started;       // Flag to check if the process has started executing
+  /* End of "Additional fields here" */
+};
+
+TAILQ_HEAD(process_list, process);
+
+u32 next_int(const char **data, const char *data_end)
+{
+  u32 current = 0;
+  bool started = false;
+  while (*data != data_end)
+  {
+    char c = **data;
+
+    if (c < 0x30 || c > 0x39)
+    {
+      if (started)
+      {
+        return current;
+      }
+    }
+    else
+    {
+      if (!started)
+      {
+        current = (c - 0x30);
+        started = true;
+      }
+      else
+      {
+        current *= 10;
+        current += (c - 0x30);
+      }
+    }
+
+    ++(*data);
+  }
+
+  printf("Reached end of file while looking for another integer\n");
+  exit(EINVAL);
+}
+
+u32 next_int_from_c_str(const char *data)
+{
+  char c;
+  u32 i = 0;
+  u32 current = 0;
+  bool started = false;
+  while ((c = data[i++]))
+  {
+    if (c < 0x30 || c > 0x39)
+    {
+      exit(EINVAL);
+    }
+    if (!started)
+    {
+      current = (c - 0x30);
+      started = true;
+    }
+    else
+    {
+      current *= 10;
+      current += (c - 0x30);
+    }
+  }
+  return current;
+}
+
+void init_processes(const char *path,
+                    struct process **process_data,
+                    u32 *process_size)
+{
+  int fd = open(path, O_RDONLY);
+  if (fd == -1)
+  {
+    int err = errno;
+    perror("open");
+    exit(err);
+  }
+
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+  {
+    int err = errno;
+    perror("stat");
+    exit(err);
+  }
+
+  u32 size = st.st_size;
+  const char *data_start = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (data_start == MAP_FAILED)
+  {
+    int err = errno;
+    perror("mmap");
+    exit(err);
+  }
+
+  const char *data_end = data_start + size;
+  const char *data = data_start;
+
+  *process_size = next_int(&data, data_end);
+
+  *process_data = calloc(sizeof(struct process), *process_size);
+  if (*process_data == NULL)
+  {
+    int err = errno;
+    perror("calloc");
+    exit(err);
+  }
+
+  for (u32 i = 0; i < *process_size; ++i)
+  {
+    (*process_data)[i].pid = next_int(&data, data_end);
+    (*process_data)[i].arrival_time = next_int(&data, data_end);
+    (*process_data)[i].burst_time = next_int(&data, data_end);
+  }
+
+  munmap((void *)data, size);
+  close(fd);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    return EINVAL;
+  }
+  struct process *data;
+  u32 size;
+  init_processes(argv[1], &data, &size);
+
+  u32 quantum_length = next_int_from_c_str(argv[2]);
+
+  struct process_list list;
+  TAILQ_INIT(&list);
+
+  u32 total_waiting_time = 0;
+  u32 total_response_time = 0;
+
+  /* Your code here */
+  struct process *current_process;
+  int current_time = 0; // Tracks the current time of the scheduler
+  int total_waiting_time = 0;
+  int total_response_time = 0;
+  int process_count = 0;
+
+  // Main scheduling loop
+  while (!TAILQ_EMPTY(&head)) {
+      current_process = TAILQ_FIRST(&head);
+      TAILQ_REMOVE(&head, current_process, entries);
+
+      // First time the process is being executed
+      if (!current_process->has_started) {
+          current_process->has_started = 1;
+          current_process->response_time = current_time - current_process->arrival_time;
+      }
+
+      // Determine the time slice for the current execution
+      int time_slice = (current_process->remaining_time > quantum) ? quantum : current_process->remaining_time;
+      current_process->remaining_time -= time_slice;
+      current_time += time_slice;
+
+      // Update waiting time for all processes in the queue
+      struct process *proc;
+      TAILQ_FOREACH(proc, &head, entries) {
+          proc->waiting_time += time_slice;
+      }
+
+      // If process is still not complete, re-enqueue it at the tail
+      if (current_process->remaining_time > 0) {
+          TAILQ_INSERT_TAIL(&head, current_process, entries);
+      } else {
+          // Compute total times for averages
+          total_waiting_time += current_process->waiting_time;
+          total_response_time += current_process->response_time;
+          process_count++;
+          free(current_process); // Free memory of completed process
+      }
+  }
+
+  // Calculate average waiting and response times
+  if (process_count > 0) {
+      printf("Average waiting time: %.2f\n", (double)total_waiting_time / process_count);
+      printf("Average response time: %.2f\n", (double)total_response_time / process_count);
+  }
+
+  /* End of "Your code here" */
+
+  printf("Average waiting time: %.2f\n", (float)total_waiting_time / (float)size);
+  printf("Average response time: %.2f\n", (float)total_response_time / (float)size);
+
+  free(data);
+  return 0;
+}
